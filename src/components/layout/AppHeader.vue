de60bdbba48ae3e362b80aeca457ac04263b4a48
Florian <palabostf@gmail.com>
2024-02-04 11:51:34 +0100

    template: dynamic routes in nav

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-    import { RouterLink } from 'vue-router';
+    import { RouteRecordRaw, RouterLink } from 'vue-router';
+    import { routes } from '@/router';
+
+    import { StringHelper } from '@/utils/StringHelper';
+
+    export interface RouteAppHeader {
+        path: string;
+        name: string;
+    }
+
+    const routeList: RouteAppHeader[] = routes.map((route: RouteRecordRaw) => {
+        return { path: route.path, name: StringHelper.ucFirst(route.name as string) } as RouteAppHeader;
+    });
 </script>
 <template>
     <header class="">
@@ -8,15 +20,16 @@
                 <RouterLink to="/" class="font-semibold text-xl">Home</RouterLink>
                 <div class="w-full block flex-grow lg-flex lg:items-center lg:w-auto" id="navbarNavAltMarkup">
                     <div class="text-sm lg:flex-grow ml-5">
-                        <RouterLink to="/about" class="mt-4 lg-inline-block lg:mt-O hover:text-white mr-4"
-                            >About</RouterLink
-                        >
-                        <RouterLink to="/sample" class="mt-4 lg-inline-block lg:mt-O hover:text-white mr-4"
-                            >Sample</RouterLink
+                        <RouterLink
+                            v-for="currentRoute in routeList"
+                            :key="currentRoute.name"
+                            :to="currentRoute.path"
+                            class="mt-4 lg-inline-block lg:mt-O hover:text-white mr-4"
+                            >{{ currentRoute.name }}</RouterLink
                         >
                     </div>
                 </div>
-                <span class="navbar-text me-auto fs-2"> Sample Header </span>
+                <span class="navbar-text me-auto fs-2"> Vue Tracker </span>
             </div>
         </nav>
     </header>
